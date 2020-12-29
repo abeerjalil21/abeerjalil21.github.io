a32b7c81fb337cda0b348de9c7383df03b0dc9b7
abeerjalil21 <72802530+abeerjalil21@users.noreply.github.com>
2020-12-29 18:39:43 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>
-          I am trying to put heading 3 tag now
-        </h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload. This is my first attempt with the coolest sir :D
+          Edit <code>src/App.js</code> and save to reload. This is my first attempt with the coolest sir :D 2nd attemp
         </p>
         <a
           className="App-link"
