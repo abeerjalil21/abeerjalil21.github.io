2b6216e0b7116635f1ee4b51673c25148798c40e
abeerjalil21 <72802530+abeerjalil21@users.noreply.github.com>
2020-12-29 18:38:49 +0500

    Adding H3 in app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h3>
+          I am trying to put heading 3 tag now
+        </h3>
         <p>
           Edit <code>src/App.js</code> and save to reload. This is my first attempt with the coolest sir :D
         </p>
